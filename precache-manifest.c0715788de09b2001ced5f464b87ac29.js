b7fa78a600cdc1c6b3ca2bbe0d9bafc944e4cc47
jy.kim <zpfhtmvlspf@gmail.com>
2020-03-10 16:42:46 +0900

    Updates

---
+++ precache-manifest.c0715788de09b2001ced5f464b87ac29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f32a584dc6a86778f7d94681b1bd4e5",
+    "url": "/react-tutorial-webtoon/index.html"
+  },
+  {
+    "revision": "efca95875e1a67555c83",
+    "url": "/react-tutorial-webtoon/static/css/main.544bd55e.chunk.css"
+  },
+  {
+    "revision": "a33057efe68d6841a0f9",
+    "url": "/react-tutorial-webtoon/static/js/2.f6f05417.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-tutorial-webtoon/static/js/2.f6f05417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efca95875e1a67555c83",
+    "url": "/react-tutorial-webtoon/static/js/main.93b53559.chunk.js"
+  },
+  {
+    "revision": "91958f7942ee4713c7f6",
+    "url": "/react-tutorial-webtoon/static/js/runtime-main.05a3d189.js"
+  }
+]);
